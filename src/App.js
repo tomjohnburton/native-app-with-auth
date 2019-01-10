5ceb5be6fff4706e18bcecd55b757da5d2a6075d
tomjohnburton <tomjohnburton@gmail.com>
2019-01-10 17:23:36 +0100

    Finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import firebase from "firebase";
-import { Header } from "./components/common";
+import { Header, Button } from "./components/common";
 import LoginForm from "./components/LoginForm";
+import Spinner from "./components/common/Spinner";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { loggedIn: false };
+    this.state = { loggedIn: null };
   }
 
   componentDidMount = () => {
@@ -31,11 +32,24 @@ export default class App extends Component {
     });
   };
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText={"Authentication"} />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
